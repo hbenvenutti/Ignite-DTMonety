38157b4224c3dcd388f199b603de9b6c5ffee63b
Huam Benvenutti <huambenvenutti@protonmail.com>
2021-11-11 02:11:25 -0300

    estilizaçao do modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,6 @@
 import Modal from 'react-modal';
+
+import closeImg from '../../assets/close.svg';
 import { Container } from './styles';
 
 interface NewTransactionModalProps {
@@ -11,14 +13,47 @@ Modal.setAppElement('#root');
 export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModalProps) {
 
   return (
-    <Container>
-      <Modal 
-        isOpen={isOpen}
-        onRequestClose={onRequestClose} 
+    <Modal 
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
+    >
+      <button 
+        type="button" 
+        onClick={onRequestClose} 
+        className="react-modal-close"
       >
-        <h2>Cadastrar Transação</h2>
-      </Modal>
-    </Container>
+        
+        <img src={closeImg} alt="Fechar modal"/>
+      </button>
+      
+      <Container>
+      
+          <h2>Cadastrar Transação</h2>
+
+          <input
+            type="text"
+            placeholder="Título"
+          />
+        
+          <input
+            type="number"
+            placeholder="Valor"
+          />
+
+          <input 
+            type="text" 
+            placeholder="Categoria"
+          />
+
+          <button type="submit">
+            Cadastrar
+          </button>
+      
+      </Container>
+
+    </Modal>
 
   );
 };
