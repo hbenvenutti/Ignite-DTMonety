998547a9e3b9cddd76202c3c129a5f4ac870722e
Huam Benvenutti <huambenvenutti@protonmail.com>
2021-11-14 18:10:38 -0300

    cores nos botoes

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -51,6 +51,7 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
               type="button"
               onClick={() => {setType('deposit')}}
               isActive={type === 'deposit'}
+              activeColor='green'
             >
               <img src={incomeImg} alt="Income"/>
               <span>Entrada</span>
@@ -60,6 +61,8 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
               type="button"
               onClick={() => { setType('withdraw') }}
               isActive={type === 'withdraw'}
+              activeColor='red'
+
             >
               <img src={outcomeImg} alt="Outcome"/>
               <span>Saída</span>
